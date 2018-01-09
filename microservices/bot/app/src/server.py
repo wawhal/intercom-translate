2ac6be3a7bd5db6585a09d40e5408929b3a841d8
wawhal <rishichandra.wawhal@gmail.com>
2018-01-09 21:41:33 +0530

    DeploymentPush

--- microservices/bot/app/src/server.py
+++ microservices/bot/app/src/server.py
@@ -74,8 +74,7 @@ def bot():
         else:
             if (langMode != 'none'):
                 respObj = googleTranslate.translate(langMode, text)
-                if (respObj["error"]):
-                    print (respObj["error"])
+                if ("error" in respObj):
                     intercom.sendNote(convId, "Invalid Language code. Please set it again.")
                     return "ok"
                 translationObj = respObj["data"]["translations"][0]
